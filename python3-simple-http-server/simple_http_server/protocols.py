9ffd3fb59ede4891d2254be6923139904b09052f
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2017-11-19 22:00:34 +0900

    refactoring redundant type hints

--- python3-simple-http-server/simple_http_server/protocols.py
+++ python3-simple-http-server/simple_http_server/protocols.py
@@ -2,19 +2,17 @@ from asyncio.streams import StreamReader, StreamWriter
 
 from .handler import handle_request
 from .parser import parse_http
-from .request import Request
-from .response import Response
 
 
 async def http(reader: StreamReader, writer: StreamWriter) -> None:
     request_line: bytes = await reader.readline()
-    if request_line is None:
+    if not request_line:
         writer.close()
 
-    data: str = request_line.decode()
-    request: Request = await parse_http(data)
+    data = request_line.decode()
+    request = await parse_http(data)
 
-    response: Response = handle_request(request)
+    response = handle_request(request)
     response.write_to(writer)
     await writer.drain()
 
